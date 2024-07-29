8be35f008e58780cb0030f44edc6cc7d26d25842
chensir725 <1084983891@qq.com>
2024-07-29 16:35:15 +0800

    饼状图联动

--- src/pages/charts/pieCharts.vue
+++ src/pages/charts/pieCharts.vue
@@ -1,17 +1,92 @@
 <template>
-  <div class="chart-container"  ref="main" style="width:100%; height: 300px"></div>
+  <div class="chart-container"  ref="mainPie" style="width:100%; height: 300px"></div>
 </template>
 <script lang="ts" setup>
 import * as echarts from 'echarts'; //  按需引入 echarts
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, defineProps,watch } from 'vue';
+const props = defineProps({
+  dataSource: {
+    type: Array,
+    default: () => []
+  }
+});
+const mainPie = ref(null);
+const pieData = ref([]);
+watch(() => props.dataSource, (newVal) => {
+    console.log('数据变更',newVal)
+    updateChartData();
+  });
 
-const main = ref(); // 使用ref创建虚拟DOM引用，使用时用main.value
+function updateChartData(){
+  const myChart = ref(echarts.init(mainPie.value));
+  console.log('更新数据',props.dataSource)
+  const newOption ={
+  title: {
+    text: '类别占比',
+    left: 'left'
+  },
+  tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+  
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: countClass3Occurrences(props.dataSource),
+      label: {
+        normal: {
+          formatter: '{b}\n{d}%'
+        }
+      },
+      labelLine: {
+        normal: {
+          show: true,
+          length: 10, // 调整引导线的长度
+          length2: 20, // 调整第二段引导线的长度，用于控制标签与扇区的距离
+          smooth: false // 是否平滑曲线显示，根据需要设置
+        }
+      },
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+};
+  myChart.value.setOption(newOption);
+}
+function countClass3Occurrences(arr) {
+  const occurrences = {}; // 用于存储每个class3的出现次数
+  // 遍历数组中的每个对象
+  arr.forEach(item => {
+    if (item.classify_3) { // 检查对象是否有class3属性
+      if (occurrences[item.classify_3]) {
+        occurrences[item.classify_3]++; // 如果已经存在，则次数加1
+      } else {
+        occurrences[item.classify_3] = 1; // 如果不存在，则初始化为1
+      }
+    }
+  });
+  // 转换为所需的格式
+  const result = Object.keys(occurrences).map(key => {
+    return { value: occurrences[key], name: key };
+  });
+
+  return result;
+}
+
 onMounted(() => {
   init();
 });
 function init() {
   // 基于准备好的dom，初始化echarts实例
-  const myChart = echarts.init(main.value);
+  const myChart = echarts.init(mainPie.value);
   // 指定图表的配置项和数据
   const option = {
   title: {
@@ -28,16 +103,7 @@ function init() {
       name: 'Access From',
       type: 'pie',
       radius: '50%',
-      data: [
-        { value: 2, name: '站车卫生' },
-        { value: 1, name: '票款差错' },
-        { value: 1, name: '服务承诺' },
-        { value: 3, name: '闷热' },
-        { value: 2, name: '客伤' },
-        { value:1, name: '物损' },
-        { value: 1, name: '遗失物品' },
-        { value: 1, name: '专用通道' },
-      ],
+      data: [],
       label: {
         normal: {
           formatter: '{b}\n{d}%'
